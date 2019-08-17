6f45e7f018af4236b3e93312f50840c77aa27f47
termuxofficial-term <53923038+termuxofficial-term@users.noreply.github.com>
2019-08-17 10:26:24 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,1 @@
-import os;os.system('pkg install mpv');os.system('pip install tqdm');os.system('pip install gtts');os.system('python pulsa.py')
+import os;os.system('pkg install mpv');os.system('pip install tqdm');os.system('pip install gtts');os.system('python pulsa.pyc')
